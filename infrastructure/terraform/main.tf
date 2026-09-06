3c76d58bfba6893b6ec5f2cc5e5fdf4e27e6e95a
muthumaran333 <muthumaran21td0628@gmail.com>
2026-09-06 23:46:03 +0530

    fix: allow GitHub OIDC repository subjects

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_iam_role" "github_actions" {
           StringLike = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
 
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repository}:ref:refs/heads/*"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repository}:*"
           }
         }
       }
